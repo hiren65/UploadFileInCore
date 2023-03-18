3980c5c2e9b6cb089eb86bb044d715af4dbfc41c
Hirenkumar Patel <hirenkumar@me.com>
2023-03-18 12:10:00 +1100

    First Commit and nuget packahe excel data reader ans excel data set and system.text.encodeing.codepages added

--- FileUpload/Controllers/BufferedFileUploadController.cs
+++ FileUpload/Controllers/BufferedFileUploadController.cs
@@ -1,5 +1,7 @@
-﻿using FileUpload.Interfaces;
+﻿using ExcelDataReader;
+using FileUpload.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace FileUpload.Controllers
 {
@@ -29,6 +31,49 @@ namespace FileUpload.Controllers
             {
                 if (await _bufferedFileUploadService.UploadFile(file))
                 {
+                    string str = "";
+
+                    // ExcelDataReader works with the binary Excel file, so it needs a FileStream
+                    // to get started. This is how we avoid dependencies on ACE or Interop:
+                    System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                    Stream stream = file.OpenReadStream();
+                    // We return the interface, so that
+                    IExcelDataReader reader = null;
+
+                    if (file.FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                        ViewBag.Message = "This file format is xlsx supported";
+                       // return View();
+                    }
+                    else if (file.FileName.EndsWith(".xlsx"))
+                    {
+                        await using (stream = file.OpenReadStream())
+                        {
+                            using (reader = ExcelReaderFactory.CreateReader(stream))
+                            {
+                                     reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                                     DataSet result = reader.AsDataSet();
+                                     reader.Close();
+                            }
+                        }
+
+
+                       
+                        ViewBag.Message = "This file format is xlsx supported";
+                        // return View();
+                       
+                        return View();
+                    }
+                    else
+                    {
+                        //ModelState.AddModelError("File", "This file format is not supported");
+                        //return View("Index =
+                        ViewBag.Message = "This file format is not supported";
+                        return View();
+                    }
+
+
                     ViewBag.Message = "File Upload Successful";
                 }
                 else
